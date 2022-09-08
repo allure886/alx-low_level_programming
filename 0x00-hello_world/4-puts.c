343acadc01c84fdf5efded7f8d66d21621b3225f
allure886 <anereobimmesoma886@gmail.com>
2022-09-08 08:51:55 -0700

    Prints out a C program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
 
 #include <stdio.h>
 
-/* Using 'puts' to print out a sentence in C */
+/** 
+ *   * Description: main - Prints out a string to stdout. 
+ *    * Return: 0 if success. 
+ *     */
 
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return(0);
+puts ("\"Programming is like building a multilingual puzzle");
+return(0);
 } 
