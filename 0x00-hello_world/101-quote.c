958b376b802baad0c1f7ec5ad32cc032db77db95
allure886 <anereobimmesoma886@gmail.com>
2022-09-08 12:32:37 -0700

    Dora Korpar

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,10 @@
-#include <stdio.h>
 #include <unistd.h>
 /**
  * main - Entry point
- * Return: always 0 if successful
+ * Return: Always 0 if successful
  */
 int main(void)
 {
-	write(2, "and that piece is useful\" - Dora Korpar, 2015-10-19\n",
-	sizeof("and that piece of art is useful\" - Dora korpar, 2015-10-19"));
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
