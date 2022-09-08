c30546fa959af7284c34971768b88e72c650ba2b
allure886 <anereobimmesoma886@gmail.com>
2022-09-08 09:39:35 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
-
-#include <stdio.h>
-
+#include <stdio.h> 
 /** 
  * main - The containing function of this program
  * Return:it gives zero if successful
  */
-
 int main(void)
 {
-       puts("\"Programming is like building a multilingual puzzle");
-       return(0);
+     	puts("\"Programming is like building a multilingual puzzle");
+       
+       	return (0);
 } 
