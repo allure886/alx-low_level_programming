7db1b867ef2793c1ea9b89fbb5fb9db2a03ec03d
allure886 <anereobimmesoma886@gmail.com>
2022-09-13 09:00:22 -0700

    Prints _putchar followed by a new line

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,17 +1,7 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
-/** definitions, enums 
- * typedefs
- * function prototypes
- */
+int _putchar(char c);
 
-#include <unistd.h>
-
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
-#endif /* _MAIN_H_ */
+#endif
 
