360e63099e2189b8c3d73db99b60d21c6e118b86
allure886 <anereobimmesoma886@gmail.com>
2022-09-15 15:12:44 -0700

    Task 10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -11,27 +11,20 @@ void print_triangle(int size)
 	if (size <= 0)
 	{
 		_putchar('\n');
-	}
-	else
+	} else
 	{
-	int i;
+		int i, j;
 
-	for (i = 0; i <= size; i++)
-	{
-		int j;
-
-		for (j = 1; j <= size; j++)
+		for (i = 1; i <= size; i++)
 		{
-			if (j <= (size - i))
+			for (j = 1; j < size; j++)
 			{
-				_putchar(' ');
-			}
-			else
-			{
-				_putchar('#');
+				for (j = 1; j <= i; j++)
+				{
+					_putchar('#');
+				}
+				_putchar('\n');
 			}
 		}
-		_putchar('\n');
-	}
 	}
 }
