7331cbb7dfaa0e64647f54593035263f71921fca
allure886 <anereobimmesoma886@gmail.com>
2022-09-15 09:44:10 -0700

    prints out triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * print_triangle - prints a triangle
+ * @size: size of character
+ * Return: 0
+ */
+
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+		_putchar('\n');
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			if (j <= (size - i))
+				_putchar(' ');
+			else
+				_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
